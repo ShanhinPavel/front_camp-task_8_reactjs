465e181628cff4bebb15ed9dce0e18b5d61429cf
Pavel Shanhin <shanMail@tut.by>
2020-01-11 22:55:44 +0300

    fix: change hardcore types in reducers with constants from constants folder

--- src/reducers/set-error.js
+++ src/reducers/set-error.js
@@ -1,6 +1,8 @@
+import { DOWNLOADING_ERROR } from '../constants';
+
 export const setError = (state = false, action) => {
   switch (action.type) {
-    case 'DOWNLOADING_ERROR':
+    case DOWNLOADING_ERROR:
       return action.error;
 
     default:
--- src/reducers/sort-by.js
+++ src/reducers/sort-by.js
@@ -1,10 +1,11 @@
 import { TAB_BUTTON_VALUES } from '../components/common/tab-buttons-section';
+import { SET_SORT_TYPE } from '../constants';
 
 const initialSortBy = TAB_BUTTON_VALUES.RELEASE_DATE;
 
 export const sortBy = function (state = initialSortBy, action) {
   switch (action.type) {
-    case 'SET_SORT_TYPE':
+    case SET_SORT_TYPE:
       return action.sortType;
   }
   return state;
