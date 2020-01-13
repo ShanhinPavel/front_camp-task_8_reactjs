0340ed5612ee236ba6c93c7255a6c17ab9c20d17
Pavel Shanhin <shanMail@tut.by>
2020-01-13 20:04:40 +0300

    feat: add spinner to application

--- src/components/common/spinner/spinner.jsx
+++ src/components/common/spinner/spinner.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { css } from '@emotion/core';
+import { BarLoader } from 'react-spinners';
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
 
 export const Spinner = () => {
+  const { isLoading } = useState((state) => state);
   return (
-    <div>
-      <h2>Spinner</h2>
+    <div style={{ margin: '0 auto' }}>
+      <BarLoader css={override} size={25} color={'#ffffff'} loading={isLoading} />
     </div>
   );
 };
