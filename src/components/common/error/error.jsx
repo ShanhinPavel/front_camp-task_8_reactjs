dfde0c065a7fe098549b1c52b2f07a0d85674dae
Pavel Shanhin <shanMail@tut.by>
2020-01-15 23:31:11 +0300

    style: center error message

--- src/components/common/error/error.jsx
+++ src/components/common/error/error.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 export const Error = () => (
-  <div>
-    <h1>Oops!.Something went wrong. Please reload the page.</h1>
+  <div style={{ margin: '50vh auto', color: 'white', textAlign: 'center' }}>
+    <h1>Oops! Something went wrong.</h1>
+    <p>Please reload the page.</p>
   </div>
 );
